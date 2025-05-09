f34209e4a1a3fce1efcab54cd30e47d4ff1504b8
Sarah Davies <sarahdavies186@gmail.com>
2025-05-09 11:03:21 +0100

    static layout and components updated

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,12 +1,8 @@
-import { Link } from "react-router-dom";
 import styles from "./Logo.module.css";
+import { Link } from "react-router-dom";
 
 function Logo() {
-  return (
-    <Link to="/">
-      <img src="/logo.png" alt="WorldWise logo" className={styles.logo} />
-    </Link>
-  );
+  return <Link to="/"><img src="/logo.png" alt="WorldWise logo" className={styles.logo} /></Link>;
 }
 
 export default Logo;
